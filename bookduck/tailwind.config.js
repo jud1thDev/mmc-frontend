54c2a4786e73a6104172058e923c678e21a6186a
chanhee <ichanhui578@gmail.com>
2024-10-10 00:27:21 +0900

    💚chore:공통 폰트 설정

--- bookduck/tailwind.config.js
+++ bookduck/tailwind.config.js
@@ -3,7 +3,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        t1: ["1.5rem", { lineHeight: "2.25rem" }],
+        t2: ["1.25rem", { lineHeight: "1.75rem" }],
+        st: ["1.125rem", { lineHeight: "1.5rem" }],
+        b1: ["1rem", { lineHeight: "1.5rem" }],
+        b2: ["0.875rem", { lineHeight: "1.5rem" }],
+        btn1: ["1.125rem", { lineHeight: "1.5rem" }],
+        btn2: ["1rem", { lineHeight: "1.25rem" }],
+        btn3: ["0.875rem", { lineHeight: "1.25rem" }],
+        btn4: ["0.75rem", { lineHeight: "1rem" }],
+        c1: ["0.75rem", { lineHeight: "1rem" }],
+        c2: ["0.6875rem", { lineHeight: "0.8125rem" }],
+      },
+      fontWeight: {
+        semibold: 600,
+        regular: 400,
+      },
+    },
   },
   plugins: [],
 };
