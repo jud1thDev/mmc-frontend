098de271c9bc9a48103f446676948f78584fdabf
YoungseoChoi23 <dudtj9326@ewhain.net>
2024-10-13 23:22:31 +0900

    💄 UI : BookListView 컴포넌트에 직접 등록한 책 텍스트 추가
    register prop의 값이 true면 "직접 등록한 책" 텍스트가 함께 보

--- bookduck/src/components/common/BookListView.jsx
+++ bookduck/src/components/common/BookListView.jsx
@@ -3,6 +3,7 @@ import menuBar from "../../assets/common/menu-bar.svg";
 import BookListViewDropdown from "./BookListViewDropdown";
 
 const BookListView = ({
+  register = true,
   bookTitle = "책제목",
   author = "지은이",
   edit = false,
@@ -11,9 +12,12 @@ const BookListView = ({
     <div className="flex items-center justify-between w-[393px] h-[124px] pl-[16px] pr-[16px] pb-[8px] pt=[8px]">
       <div className="flex gap-[12px] items-center w-[252px] h-[108px]">
         <img src={coverEx} alt="coverEx" />
-        <div className="flex flex-col">
-          <div className="h-[24px] text-c1 text-gray-800">{bookTitle}</div>
-          <div className="h-[24px] text-c1 text-gray-500">{author}</div>
+        <div className="flex flex-col gap-1">
+          {register && (
+            <div className=" text-c1 text-orange-400">직접 등록한 책</div>
+          )}
+          <div className="text-b1 text-gray-800">{bookTitle}</div>
+          <div className=" text-b1 text-gray-500">{author}</div>
         </div>
       </div>
       {edit ? (
