2fc7d8c9c0c6de1a07ec1dc2f2f39aa829cdc63d
YoungseoChoi23 <dudtj9326@ewhain.net>
2024-10-13 23:11:40 +0900

    💄 UI : textfield 컴포넌트 UI 수정
    TextField 컴포넌트 props => type : "내용"이면 인풋필드 위에 제목 적을 수 있는 공간 생성. 그 외에는 인풋필드만 보임, title:  type이 "내용"일 경우 title값 입력, placeholder: input의 placeholder 입력, check : input 필드에 확인 버튼이 필요한 경우, handleEdit : 확인 버튼 클릭 시 연결되는 함수 입력, error : 확인 함수 결과 에러 유무 t/f로 전송, isSubmitted : 확인 버튼 클릭 시 true로  상태 바꿔야 함, defaultType : 값이 true인 경우 error msg가 title옆에 위치, true가 아니면 input field 밑에 위치하게 됨

--- bookduck/src/App.jsx
+++ bookduck/src/App.jsx
@@ -1,10 +1,32 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import TextField from "./components/common/TextField";
+import { useState } from "react";
 function App() {
+  const [error, setError] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleEdit = () => {
+    setError(true);
+    setIsSubmitted(true);
+  };
   return (
     <Routes>
-      <Route path="/" element={<TextField type="내용" title="제목" />} />
+      <Route
+        path="/"
+        element={
+          <TextField
+            type="내용"
+            placeholder="이메일입력"
+            title="로그인정보"
+            check={false}
+            handleEdit={handleEdit}
+            error={error}
+            isSubmitted={isSubmitted}
+            defaultType={true}
+          />
+        }
+      />
     </Routes>
   );
 }
--- bookduck/src/components/common/TextField.jsx
+++ bookduck/src/components/common/TextField.jsx
@@ -1,19 +1,30 @@
 import { useState } from "react";
 //textfield의 type이 제목 field면 type=="제목", 내용 field면 type=="내용"
 //내용 type의 경우 title을 prop으로 입력 받음
-//수정 가능한 textfield의 경우 edit 값이 true, 이 경우 수정 관련 로직을 error, handleEdit로 넘겨줌
-
-const TextField = ({ type, title, edit, error, handleEdit }) => {
+//확인 가능한 textfield의 경우 edit 값이 true, 이 경우 확인 관련 로직을 error, handleEdit, isSubmitted로 넘겨줌
+//handleEdit에서 유효성 검사를 한 후 error값을 prop으로 넘겨줌
+//handleEdit 함수 수행 시에는 isSubmitted를 true값으로 바꿔 상태 업데이트 (default 상태와, 확인 후 error==true / false 상태를 구분하기 위함)
+//defaultType의 경우 에러메시지가 input 아래에 생기는 게 아니라 title 옆에 생김
+const TextField = ({
+  type,
+  title,
+  placeholder,
+  check = true,
+  error,
+  handleEdit,
+  isSubmitted,
+  defaultType,
+}) => {
   const [inputValue, setInputValue] = useState("");
   const handleValue = (e) => {
     setInputValue(e.target.value);
   };
   return (
     <>
-      {type == "내용" && (
+      {type === "내용" && (
         <div className="flex gap-[8px]">
           <div className="h-[16px] text-c1 text-gray-400">{title}</div>
-          {error && (
+          {defaultType && error && (
             <div className="text-c1 text-orange-600">필수입력란입니다!</div>
           )}
         </div>
@@ -22,22 +33,30 @@ const TextField = ({ type, title, edit, error, handleEdit }) => {
         <input
           value={inputValue}
           onChange={handleValue}
-          placeholder={type}
+          placeholder={placeholder}
           className={`w-[361px] h-[48px] px-[4px] py-[12px]  ${
-            error
+            !isSubmitted
+              ? "border-b-[1px] border-gray-200"
+              : error === true
               ? "border-b-[1px] border-orange-600"
-              : "border-b-[1px] border-gray-200"
+              : "border-b-[1px] border-blue"
           } text-b1 text-gray-800`}
         />
-        {!edit && (
+        {check && (
           <div
             onClick={handleEdit}
-            className="absolute top-[16px] right-[40px] text-c1 text-orange-400 cursor-pointer"
+            className="absolute top-[16px] right-[40px] text-b2 text-gray-600 cursor-pointer underline underline-offset-4"
           >
-            수정하기
+            확인
           </div>
         )}
       </div>
+      {/* 확인 버튼이 클릭된 경우에만 메시지 표시 */}
+      {!defaultType && isSubmitted && (
+        <div className={`text-c1 ${error ? "text-orange-600" : "text-blue"}`}>
+          {error ? "이미 사용 중인 이름입니다!" : "사용 가능한 이름입니다!"}
+        </div>
+      )}
     </>
   );
 };
