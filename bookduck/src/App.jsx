e5a1a1442a8b783870443011912b812a04fa7734
YoungseoChoi23 <dudtj9326@ewhain.net>
2024-10-13 23:12:50 +0900

    ✏️ Fix : App.jsx 파일 원래대로 수정

--- bookduck/src/App.jsx
+++ bookduck/src/App.jsx
@@ -3,30 +3,9 @@ import HomePage from "./pages/HomePage";
 import TextField from "./components/common/TextField";
 import { useState } from "react";
 function App() {
-  const [error, setError] = useState(false);
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
-  const handleEdit = () => {
-    setError(true);
-    setIsSubmitted(true);
-  };
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          <TextField
-            type="내용"
-            placeholder="이메일입력"
-            title="로그인정보"
-            check={false}
-            handleEdit={handleEdit}
-            error={error}
-            isSubmitted={isSubmitted}
-            defaultType={true}
-          />
-        }
-      />
+      <Route path="/" element={<HomePage />} />
     </Routes>
   );
 }
