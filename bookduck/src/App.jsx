4dd81426c02461b1a37c94bac3c004d0b8bbe4d9
chanhee <ichanhui578@gmail.com>
2024-10-21 22:28:43 +0900

    💄ui:FriendPage추가

--- bookduck/src/App.jsx
+++ bookduck/src/App.jsx
@@ -1,9 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import SettingPage from "./pages/Setting/SettingPage";
+import NotificationPage from "./pages/Notification/NotificationPage";
+import FriendListPage from "./pages/Friend/FriendListPage";
+import FriendRequestPage from "./pages/Friend/FriendRequestPage";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<SettingPage />} />
+      <Route path="/notification" element={<NotificationPage />} />
+      <Route path="/friend/list" element={<FriendListPage />} />
+      <Route path="/friend/request" element={<FriendRequestPage />} />
     </Routes>
   );
 }
