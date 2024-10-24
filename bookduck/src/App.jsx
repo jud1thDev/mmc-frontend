d7237372bc2bf2cd6478a6b0c942c65a8e532f08
yoonjin-C <cyy000123@gmail.com>
2024-10-25 03:34:25 +0900

    ✨ Feat: CharacterPage 라우팅 추가

--- bookduck/src/App.jsx
+++ bookduck/src/App.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import CharacterPage from "./pages/CharacterPage/CharacterPage";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/character" element={<CharacterPage />} />
     </Routes>
   );
 }
