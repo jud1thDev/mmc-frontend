96c04b2dd286d2aeb30e6f6f4a78a74ea64a3ef5
chanhee <ichanhui578@gmail.com>
2024-10-25 20:27:00 +0900

    💄ui: 컴포넌트 및 스타일 추가

--- bookduck/src/App.jsx
+++ bookduck/src/App.jsx
@@ -2,12 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import NotificationPage from "./pages/Notification/NotificationPage";
 import FriendListPage from "./pages/Friend/FriendListPage";
 import FriendRequestPage from "./pages/Friend/FriendRequestPage";
+import SearchMainPage from "./pages/Search/SearchMainPage";
+import SearchBookPage from "./pages/Search/SearchBookPage";
+import SearchArchivePage from "./pages/Search/SearchArchivePage";
+import SearchUserPage from "./pages/Search/SearchUserPage";
+import RegisterPage from "./pages/Search/RegisterPage";
 function App() {
   return (
     <Routes>
       <Route path="/notification" element={<NotificationPage />} />
       <Route path="/friend/list" element={<FriendListPage />} />
       <Route path="/friend/request" element={<FriendRequestPage />} />
+      <Route path="/search" element={<SearchMainPage />} />
+      <Route path="/search/book" element={<SearchBookPage />} />
+      <Route path="/search/archive" element={<SearchArchivePage />} />
+      <Route path="/search/user" element={<SearchUserPage />} />
+      <Route path="/search/register" element={<RegisterPage />} />
     </Routes>
   );
 }
