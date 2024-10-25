b067637427575a639455e248ed77ce555f6b27d6
YoungseoChoi23 <dudtj9326@ewhain.net>
2024-10-26 03:21:24 +0900

    💄 UI : 읽고 있어요 페이지 퍼블리싱

--- bookduck/src/App.jsx
+++ bookduck/src/App.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import RecordingPage from "./pages/RecordingPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<RecordingPage />} />
     </Routes>
   );
 }
---
+++ bookduck/src/pages/RecordingPage.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import Header3 from "../components/common/Header3";
+import { recordTab } from "../constant/recordTab";
+import Archiving from "../components/common/RecordingPage/Archiving";
+
+const RecordingPage = () => {
+  const [tab, setTab] = useState(0);
+
+  const handleTab = (index) => {
+    setTab(index);
+  };
+  return (
+    <>
+      <div className="pt-[3.69rem]">
+        <Header3 title="기록할 책 선택" />
+        <div className="flex  justify-center w-full h-[2.75rem] ml-[1rem] mr-[1rem] mb-[0.75rem] items-center">
+          {recordTab.map((it) => (
+            <div>
+              <div
+                onClick={() => handleTab(it.id)}
+                className={`flex justify-center items-center w-[5.64rem] h-[2.5rem] text-b1  ${
+                  tab === it.id ? "font-semibold" : "text-gray-400"
+                } cursor-pointer`}
+              >
+                {it.name}
+              </div>
+              {tab === it.id && (
+                <div className=" w-[4rem] m-auto border-t-[0.1375rem] border-gray-800"></div>
+              )}
+            </div>
+          ))}
+        </div>
+        {tab === 0 && <Archiving />}
+      </div>
+    </>
+  );
+};
+export default RecordingPage;
