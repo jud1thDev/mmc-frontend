ad471cd9207b9a897d1281f50ffc6896f9d65adb
YoungseoChoi23 <dudtj9326@ewhain.net>
2024-10-26 03:55:40 +0900

    ✏️ Fix : 루트 경로 수정

--- bookduck/src/App.jsx
+++ bookduck/src/App.jsx
@@ -1,11 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import RecordingPage from "./pages/RecordingPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<RecordingPage />} />
+      <Route path="/" element={<HomePage />} />
     </Routes>
   );
 }
