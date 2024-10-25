64b4bcd77c86f8dbc2ada959c55201e4d68c51bf
chanhee <ichanhui578@gmail.com>
2024-10-25 20:25:51 +0900

    💄ui: Divider 색상 수정

--- bookduck/src/components/common/Divider1.jsx
+++ bookduck/src/components/common/Divider1.jsx
@@ -1,4 +1,4 @@
 const Divider1 = () => {
-  return <div className="w-[393px] h-[8px] bg-gray-200"></div>;
+  return <div className="w-[393px] h-[8px] bg-[#FBFBFB]"></div>;
 };
 export default Divider1;
