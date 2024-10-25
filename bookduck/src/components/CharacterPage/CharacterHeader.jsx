1fc384b26240c5ee3d9904e1ac529f903378a74a
yoonjin-C <cyy000123@gmail.com>
2024-10-26 00:07:53 +0900

    💄 UI: 캐릭터 헤더바 퍼블리싱

---
+++ bookduck/src/components/CharacterPage/CharacterHeader.jsx
@@ -0,0 +1,13 @@
+import costumeIcon from "../../assets/CharacterPage/costume.svg";
+const CharacterHeader = () => {
+  return (
+    <div className="flex justify-between items-center px-4 py-1 w-[393px] h-[52px]">
+      <div className="text-t2 font-semibold">캐릭터</div>
+      <div className="flex items-center px-4 py-2 gap-1 bg-orange-50 rounded-[100px] ">
+        <img className="w-5 h-5" src={costumeIcon} alt="costume Icon" />
+        <div className="text-b2 font-semibold text-orange-400">꾸미기</div>
+      </div>
+    </div>
+  );
+};
+export default CharacterHeader;
