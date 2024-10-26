4cb81337454dbe660d9db245fb88978a6aa097fe
yoonjin-C <cyy000123@gmail.com>
2024-10-27 03:55:06 +0900

    💚 Chore: 캐릭터 꾸미기 페이지 라우팅 추가

--- bookduck/src/App.jsx
+++ bookduck/src/App.jsx
@@ -2,12 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CharacterPage from "./pages/CharacterPage/CharacterPage";
 import MyBadgePage from "./pages/CharacterPage/MyBadgePage";
+import CharacterCustomPage from "./pages/CharacterPage/CharacterCustomPage";
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/character" element={<CharacterPage />} />
       <Route path="/myBadge" element={<MyBadgePage />} />
+      <Route path="/character/custom" element={<CharacterCustomPage />} />
     </Routes>
   );
 }
