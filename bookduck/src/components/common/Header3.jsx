8d446f0a2c1f99f806e8506a752f6ae2a7a9c58f
yoonjin-C <cyy000123@gmail.com>
2024-10-29 01:52:01 +0900

    ✨ Feat: 뒤로가기 버튼 라우팅 추가

--- bookduck/src/components/common/Header3.jsx
+++ bookduck/src/components/common/Header3.jsx
@@ -1,16 +1,19 @@
-
+import { useNavigate } from "react-router-dom";
 import header_arrow from "../../assets/common/header-arrow.svg";
 
 import setting from "../../assets/common/setting.svg";
 import alarm from "../../assets/common/alarm.svg";
 
 const Header3 = ({ title = "제목" }) => {
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1);
+  };
   return (
     <div className="flex items-center w-full h-[2.75rem]">
-
-      <div className="ml-[1rem]">
+      <div className="ml-[1rem] cursor-pointer" onClick={handleBackClick}>
         <img src={header_arrow} alt="arrow" />
-
       </div>
       <div className="text-st ml-[1.26rem] font-semibold">{title}</div>
     </div>
