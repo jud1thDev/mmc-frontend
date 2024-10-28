c92fb791c7c7ad2c66eb87a48d0d8dba67b2d843
chanhee <ichanhui578@gmail.com>
2024-10-28 22:48:49 +0900

    💄ui: 메인 페이지 틀 생성

---
+++ bookduck/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BottomNavbar from "../../components/common/BottomNavbar";
+const MainPage = () => {
+  return (
+    <>
+      <div>메인</div>
+      <BottomNavbar />
+    </>
+  );
+};
+
+export default MainPage;
