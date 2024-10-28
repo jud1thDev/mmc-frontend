e939a15a050125fcb01437b9a4324e063b31c025
yoonjin-C <cyy000123@gmail.com>
2024-10-29 01:49:01 +0900

    ✏️ Fix: 한줄평 모두보기 페이지 라우팅 추가

--- bookduck/src/App.jsx
+++ bookduck/src/App.jsx
@@ -3,12 +3,14 @@ import LoginPage from "./pages/LoginPage/LoginPage";
 import SigninPage from "./pages/LoginPage/SigninPage";
 import NotificationPage from "./pages/NotificationPage/NotificationPage";
 import SearchBookPage from "./pages/SearchPage/SearchBookPage";
+import CharacterPage from "./pages/CharacterPage/CharacterPage";
 import MyBadgePage from "./pages/CharacterPage/MyBadgePage";
 import CharacterCustomPage from "./pages/CharacterPage/CharacterCustomPage";
 import SelectBookPage from "./pages/RecordingPage/SelectBookPage";
 import ArchivePage from "./pages/RecordingPage/ArchivePage";
 import ArchiveDetail from "./pages/RecordingPage/ArchiveDetailPage";
-
+import BookInfoPage from "./pages/BookInfoPage/BookInfoPage";
+import UserCommentPage from "./pages/BookInfoPage/UserCommentPage";
 function App() {
   return (
     <Routes>
@@ -17,13 +19,15 @@ function App() {
       <Route path="/extract-archive-detail" element={<ArchiveDetail />} />
       <Route path="/review-archive-detail" element={<ArchiveDetail />} />
       <Route path="/total-archive-detail" element={<ArchiveDetail />} />
-
+      <Route path="/character" element={<CharacterPage />} />
       <Route path="/myBadge" element={<MyBadgePage />} />
       <Route path="/character/custom" element={<CharacterCustomPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signin" element={<SigninPage />} />
       <Route path="/notification" element={<NotificationPage />} />
       <Route path="/search/book" element={<SearchBookPage />} />
+      <Route path="/info/book" element={<BookInfoPage />} />
+      <Route path="/info/book/comment" element={<UserCommentPage />} />
     </Routes>
   );
 }
