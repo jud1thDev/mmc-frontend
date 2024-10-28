654ecd1cc3e98db0930d7c03b10e67b2150d84b6
chanhee <ichanhui578@gmail.com>
2024-10-28 22:47:57 +0900

    💚chore: bottom navBar 경로 추가

--- bookduck/src/components/common/BottomNavbar.jsx
+++ bookduck/src/components/common/BottomNavbar.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import main from "../../assets/common/bottom-nav-main.svg";
 import main_active from "../../assets/common/bottom-nav-main-active.svg";
 import search from "../../assets/common/bottom-nav-search.svg";
@@ -9,41 +10,43 @@ import character_active from "../../assets/common/bottom-nav-character-active.sv
 import library from "../../assets/common/bottom-nav-library.svg";
 import library_active from "../../assets/common/bottom-nav-library-active.svg";
 
-import { useState } from "react";
-
 const BottomNavbar = () => {
-  const [bar, setBar] = useState(0);
-  const handleMenu = (index) => {
-    setBar(index);
-  };
   return (
-    <div className="fixed bottom-0 w-[24.5625rem] h-[5.375rem] border-t-[0.3px] border-[#00000030] bg-[#FFFFFF]">
+    <div className="fixed bottom-0 w-[24.5625rem] h-[5.375rem] border-t-[0.3px] border-[#0000004D] bg-[#FFFFFF]">
       <div className="flex justify-center items-center gap-[1.89rem] mt-[0.62rem] cursor-pointer">
-        <img
-          src={bar === 0 ? main_active : main}
-          alt="main"
-          onClick={() => handleMenu(0)}
-        />
-        <img
-          src={bar === 1 ? search_active : search}
-          alt="search"
-          onClick={() => handleMenu(1)}
-        />
-        <img
-          src={bar === 2 ? record_active : record}
-          alt="record"
-          onClick={() => handleMenu(2)}
-        />
-        <img
-          src={bar === 3 ? character_active : character}
-          alt="character"
-          onClick={() => handleMenu(3)}
-        />
-        <img
-          src={bar === 4 ? library_active : library}
-          alt="library"
-          onClick={() => handleMenu(4)}
-        />
+        <NavLink to="/">
+          {({ isActive }) => (
+            <>{isActive ? <img src={main_active} /> : <img src={main} />}</>
+          )}
+        </NavLink>
+        <NavLink to="/search">
+          {({ isActive }) => (
+            <>{isActive ? <img src={search_active} /> : <img src={search} />}</>
+          )}
+        </NavLink>
+        <NavLink to="/archive">
+          {({ isActive }) => (
+            <>{isActive ? <img src={record_active} /> : <img src={record} />}</>
+          )}
+        </NavLink>
+        <NavLink to="/character">
+          {({ isActive }) => (
+            <>
+              {isActive ? (
+                <img src={character_active} />
+              ) : (
+                <img src={character} />
+              )}
+            </>
+          )}
+        </NavLink>
+        <NavLink to="/library">
+          {({ isActive }) => (
+            <>
+              {isActive ? <img src={library_active} /> : <img src={library} />}
+            </>
+          )}
+        </NavLink>
       </div>
     </div>
   );
