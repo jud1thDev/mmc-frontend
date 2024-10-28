28324b6ab1b1532e0610990c72afd6fc1d393776
yoonjin-C <cyy000123@gmail.com>
2024-10-29 02:40:25 +0900

    💄 UI: 한줄평 작성 모달창 퍼블리싱

--- bookduck/src/pages/BookInfoPage/BookInfoPage.jsx
+++ bookduck/src/pages/BookInfoPage/BookInfoPage.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import Header3 from "../../components/common/Header3";
-import starNo from "../../assets/bookinfoPage/star-no.svg";
 import BookInfo from "../../components/BookInfoPage/BookInfo";
 import TabBarComponent from "../../components/common/TabBarComponent";
 import InfoView from "../../components/BookInfoPage/InfoView";
 import ArchiveView from "../../components/BookInfoPage/ArchiveView";
 import FloatingRecordButton from "../../components/RecordingPage/FloatingRecordButton";
+import MyComment from "../../components/BookInfoPage/MyComment";
 const BookInfoPage = () => {
   const [activeTab, setActiveTab] = useState("책 정보");
   return (
@@ -15,18 +15,7 @@ const BookInfoPage = () => {
         <div className="flex flex-col gap-2 px-4">
           <div className="flex flex-col gap-5">
             <BookInfo />
-            <div className="flex flex-col items-center px-4 pt-4 pb-5 gap-2 bg-gray-10 w-[361px] rounded-lg">
-              <div className="flex items-center gap-1">
-                <img src={starNo} />
-                <img src={starNo} />
-                <img src={starNo} />
-                <img src={starNo} />
-                <img src={starNo} />
-              </div>
-              <div className="px-1 py-2 text-b2 text-gray-400 border-b border-[#DDDDDD] w-full">
-                책에 대한 나의 한줄 평을 작성해주세요!
-              </div>
-            </div>
+            <MyComment />
           </div>
           <TabBarComponent
             tabs={["책 정보", "기록"]}
