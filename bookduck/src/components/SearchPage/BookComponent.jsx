e843cc47f1dec23f01481812481515c9ff67cde7
chanhee <ichanhui578@gmail.com>
2024-10-28 22:47:23 +0900

    💄ui: book component 디자인 수정

--- bookduck/src/components/SearchPage/BookComponent.jsx
+++ bookduck/src/components/SearchPage/BookComponent.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const BookComponent = ({ img, title, writer }) => {
   return (
-    <div className="flex flex-col gap-2">
-      <img src={img} className="w-[6.5rem] h-[9.25rem]" />
+    <div className="flex flex-col gap-2 w-[6.5rem] h-fit">
+      <img src={img} className="w-full h-[9.25rem]" />
       <div>
         <div>{title}</div>
         {writer && <div>{writer}</div>}
