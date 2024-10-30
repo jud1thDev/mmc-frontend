94447bb0bc2b4f6e072f2a62f951f1243b7d907c
yoonjin-C <cyy000123@gmail.com>
2024-10-30 23:06:03 +0900

    🐛 Fix: 폰트 추가 및 한줄평 디자인 수정

--- bookduck/src/components/BookInfoPage/MyComment.jsx
+++ bookduck/src/components/BookInfoPage/MyComment.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import starNo from "../../assets/bookinfoPage/star-no.svg";
+import menu from "../../assets/bookinfoPage/menu-vertical.svg";
 import BottomSheetModal from "../../components/common/BottomSheetModal";
 import ButtonComponent from "../common/ButtonComponent";
 import BottomSheetModal2 from "./BottomSheetModal2";
+
 const MyComment = () => {
   const [bottomSheetShow, setBottomSheetShow] = useState(false);
   const [bottomSheet2Show, setBottomSheet2Show] = useState(false);
@@ -33,30 +35,58 @@ const MyComment = () => {
 
   return (
     <div>
-      <div className="flex flex-col items-center px-4 pt-4 pb-5 gap-2 bg-gray-10 w-[361px] rounded-lg">
-        <div className="flex items-center gap-1">
-          <img src={starNo} />
-          <img src={starNo} />
-          <img src={starNo} />
-          <img src={starNo} />
-          <img src={starNo} />
+      {text ? (
+        <div>
+          <div className="relative flex flex-col items-center p-5 gap-4 bg-gray-10 w-[361px] rounded-lg">
+            <div className="flex items-center gap-1">
+              <img src={starNo} />
+              <img src={starNo} />
+              <img src={starNo} />
+              <img src={starNo} />
+              <img src={starNo} />
+            </div>
+            <div className="flex justify-center items-center max-h-12 text-b2 gap-2">
+              <span
+                className="text-t2 w-[0.875rem] h-[1.25rem] text-gray-500"
+                style={{ fontFamily: "Caprasimo" }}
+              >
+                "
+              </span>
+              {text}
+              <span
+                className="text-t2 w-[0.875rem] h-[1.25rem] text-gray-500"
+                style={{ fontFamily: "Caprasimo" }}
+              >
+                "
+              </span>
+            </div>
+            <img
+              className="absolute top-5 right-5 cursor-pointer"
+              src={menu}
+              onClick={() => setBottomSheet2Show(true)}
+            />
+          </div>
         </div>
-        {text ? (
-          <div
-            className="max-h-12 text-b2 text-gray-500"
-            onClick={() => setBottomSheet2Show(true)}
-          >
-            {text}
+      ) : (
+        <div>
+          <div className="flex flex-col items-center px-4 pt-4 pb-5 gap-2 bg-gray-10 w-[361px] rounded-lg">
+            <div className="flex items-center gap-1">
+              <img src={starNo} />
+              <img src={starNo} />
+              <img src={starNo} />
+              <img src={starNo} />
+              <img src={starNo} />
+            </div>
+            <textfield
+              className="px-1 py-2 text-b2 text-gray-400 border-b border-[#DDDDDD] w-full"
+              onClick={() => setBottomSheetShow(true)}
+            >
+              책에 대한 나의 한줄 평을 작성해주세요!
+            </textfield>
           </div>
-        ) : (
-          <div
-            className="px-1 py-2 text-b2 text-gray-400 border-b border-[#DDDDDD] w-full"
-            onClick={() => setBottomSheetShow(true)}
-          >
-            책에 대한 나의 한줄 평을 작성해주세요!
-          </div>
-        )}
-      </div>
+        </div>
+      )}
+
       <BottomSheetModal
         bottomSheetShow={bottomSheetShow}
         setBottomSheetShow={setBottomSheetShow}
