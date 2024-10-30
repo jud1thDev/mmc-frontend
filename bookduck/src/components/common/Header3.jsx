e3cee227220fd17e71619a7b816b7d75d4f7f7f5
YoungseoChoi23 <dudtj9326@ewhain.net>
2024-10-30 17:38:50 +0900

    🐛 Fix : Header3 에러 수정

--- bookduck/src/components/common/Header3.jsx
+++ bookduck/src/components/common/Header3.jsx
@@ -3,16 +3,19 @@ import header_arrow from "../../assets/common/header-arrow.svg";
 
 //check = true면 취소 / 완료 버튼 표시
 
-const Header3 = ({ title = "제목" }) => {
+const Header3 = ({ title = "제목", check = false }) => {
   const navigate = useNavigate();
 
   const handleBackClick = () => {
     navigate(-1);
   };
   return (
-    <div className="flex items-center w-full h-[2.75rem]">
-      <div className="ml-[1rem] cursor-pointer" onClick={handleBackClick}>
-        <img src={header_arrow} alt="arrow" />
+    <div className="flex justify-between items-center h-[2.75rem] mx-4">
+      <div className="flex items-center">
+        <div className="cursor-pointer">
+          <img onClick={handleBackClick} src={header_arrow} alt="arrow" />
+        </div>
+        <div className="text-st ml-[1.26rem] font-semibold">{title}</div>
       </div>
       {check && (
         <div className="flex gap-6">
