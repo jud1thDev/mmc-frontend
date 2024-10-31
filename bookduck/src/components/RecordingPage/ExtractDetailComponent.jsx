578d1a9964157162643376ac214c5fb45daa3abf
yoonjin-C <cyy000123@gmail.com>
2024-11-01 04:27:53 +0900

    💄 UI: 기록 내보내기 퍼블리싱

--- bookduck/src/components/RecordingPage/ExtractDetailComponent.jsx
+++ bookduck/src/components/RecordingPage/ExtractDetailComponent.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
 import heart_gray from "../../assets/recordingPage/heart-gray.svg";
 import share_gray from "../../assets/recordingPage/share-gray.svg";
+import kakao from "../../assets/recordingPage/kakao.svg";
+import instagram from "../../assets/recordingPage/instagram.svg";
+import x from "../../assets/recordingPage/x.svg";
+import gallery from "../../assets/recordingPage/gallery.svg";
+import link from "../../assets/recordingPage/link.svg";
 
+import BottomSheetModal from "../common/BottomSheetModal";
+import BottomSheetMenuComponent from "../common/BottomSheetMenuComponent";
 const ExtractDetailComponent = () => {
+  const [bottomSheetShow, setBottomSheetShow] = useState(false);
+  const [visible, setVisible] = useState(false);
+
+  const handleCancleClick = () => {
+    setVisible(false);
+    setTimeout(() => {
+      setBottomSheetShow(false);
+    }, 200);
+  };
   return (
     <div>
       <div className="w-[22.5825rem]  px-5 py-6 rounded-[0.88rem] bg-gray-10 shadow-custom  ">
@@ -17,10 +34,58 @@ const ExtractDetailComponent = () => {
           </div>
           <div className=" flex gap-4 justify-end">
             <img className="cursor-pointer" src={heart_gray} alt="heart_gray" />
-            <img className="cursor-pointer" src={share_gray} alt="share_gray" />
+            <img
+              className="cursor-pointer"
+              src={share_gray}
+              alt="share_gray"
+              onClick={() => {
+                setBottomSheetShow(true);
+              }}
+            />
           </div>
         </div>
       </div>
+      <BottomSheetModal
+        bottomSheetShow={bottomSheetShow}
+        setBottomSheetShow={setBottomSheetShow}
+        visible={visible}
+        setVisible={setVisible}
+      >
+        <div className="flex flex-col">
+          <div className="flex justify-between items-center">
+            <span className="text-st font-semibold">내보내기</span>
+            <span
+              className="text-b1 text-gray-500 cursor-pointer"
+              onClick={handleCancleClick}
+            >
+              취소
+            </span>
+          </div>
+          <div className="flex flex-col my-4 px-1">
+            <BottomSheetMenuComponent
+              img={kakao}
+              text="카카오톡"
+              isExported={true}
+            />
+            <BottomSheetMenuComponent
+              img={instagram}
+              text="인스타그램 스토리"
+              isExported={true}
+            />
+            <BottomSheetMenuComponent img={x} text="X" isExported={true} />
+            <BottomSheetMenuComponent
+              img={gallery}
+              text="이미지 저장"
+              isExported={true}
+            />
+            <BottomSheetMenuComponent
+              img={link}
+              text="URL 복사"
+              isExported={true}
+            />
+          </div>
+        </div>
+      </BottomSheetModal>
     </div>
   );
 };
--- bookduck/src/components/RecordingPage/ReviewDetailComponent.jsx
+++ bookduck/src/components/RecordingPage/ReviewDetailComponent.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 import heart_white from "../../assets/recordingPage/heart-white.svg";
 import share_white from "../../assets/recordingPage/share-white.svg";
+import kakao from "../../assets/recordingPage/kakao.svg";
+import instagram from "../../assets/recordingPage/instagram.svg";
+import x from "../../assets/recordingPage/x.svg";
+import gallery from "../../assets/recordingPage/gallery.svg";
+import link from "../../assets/recordingPage/link.svg";
+
+import BottomSheetModal from "../common/BottomSheetModal";
+import BottomSheetMenuComponent from "../common/BottomSheetMenuComponent";
 
 const ReviewDetailComponent = () => {
+  const [bottomSheetShow, setBottomSheetShow] = useState(false);
+  const [visible, setVisible] = useState(false);
+
+  const handleCancleClick = () => {
+    setVisible(false);
+    setTimeout(() => {
+      setBottomSheetShow(false);
+    }, 200);
+  };
   return (
     <div>
       <div className="w-[22.5825rem]  px-5 py-6 rounded-[0.88rem] bg-gray-400 shadow-custom ">
@@ -39,10 +58,54 @@ const ReviewDetailComponent = () => {
               className="cursor-pointer"
               src={share_white}
               alt="share_gray"
+              onClick={() => {
+                setBottomSheetShow(true);
+              }}
             />
           </div>
         </div>
       </div>
+      <BottomSheetModal
+        bottomSheetShow={bottomSheetShow}
+        setBottomSheetShow={setBottomSheetShow}
+        visible={visible}
+        setVisible={setVisible}
+      >
+        <div className="flex flex-col">
+          <div className="flex justify-between items-center">
+            <span className="text-st font-semibold">내보내기</span>
+            <span
+              className="text-b1 text-gray-500 cursor-pointer"
+              onClick={handleCancleClick}
+            >
+              취소
+            </span>
+          </div>
+          <div className="flex flex-col my-4 px-1">
+            <BottomSheetMenuComponent
+              img={kakao}
+              text="카카오톡"
+              isExported={true}
+            />
+            <BottomSheetMenuComponent
+              img={instagram}
+              text="인스타그램 스토리"
+              isExported={true}
+            />
+            <BottomSheetMenuComponent img={x} text="X" isExported={true} />
+            <BottomSheetMenuComponent
+              img={gallery}
+              text="이미지 저장"
+              isExported={true}
+            />
+            <BottomSheetMenuComponent
+              img={link}
+              text="URL 복사"
+              isExported={true}
+            />
+          </div>
+        </div>
+      </BottomSheetModal>
     </div>
   );
 };
