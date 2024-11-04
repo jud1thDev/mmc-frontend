177a64ac759d234cd6c804732cf6a54483db4869
chanhee <ichanhui578@gmail.com>
2024-11-04 17:58:21 +0900

    ✏️fix: 주소 오류 수정

--- bookduck/src/api/api.js
+++ bookduck/src/api/api.js
@@ -2,14 +2,14 @@ import axios from "axios";
 
 /*인증 필요 없는 요청*/
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_SERVER_URL}`,
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
 });
 
 /*인증 필요한 요청*/
 const token = JSON.parse(localStorage.getItem("token"));
 const auth = token ? `Bearer ${token.accessToken}` : null;
 const apiAuth = axios.create({
-  baseURL: `${import.meta.env.VITE_SERVER_URL}`,
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
   headers: { Authorization: auth },
   withCredentials: true,
 });
