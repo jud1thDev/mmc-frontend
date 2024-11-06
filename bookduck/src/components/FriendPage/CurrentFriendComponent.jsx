73da7c566aec0b6888904bf5c2325e9ba9ca0cc5
chanhee <ichanhui578@gmail.com>
2024-11-06 21:51:38 +0900

    ✨friendPage 친구 api연결

--- bookduck/src/components/FriendPage/CurrentFriendComponent.jsx
+++ bookduck/src/components/FriendPage/CurrentFriendComponent.jsx
@@ -1,14 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import FriendListComponent from "../common/FriendListComponent";
+import { getFriendList, deleteFriend } from "../../api/friend";
+const CurrentFriendComponent = () => {
+  const [friendList, setFriendList] = useState([]);
+  const [friendCount, setFriendCount] = useState(0);
+  //API연결
+  //API-친구 목록 조회
+  const readfriendList = async () => {
+    try {
+      const response = await getFriendList();
+      setFriendList(response.data.friendList);
+      setFriendCount(response.data.friendCount);
+      console.log(response.data.friendList);
+      console.log(response.data.friendCount);
+    } catch (error) {
+      console.error("친구 정보 읽기 오류", error);
+    }
+  };
+  //API-친구 삭제
+  const delFriend = async (friendId) => {
+    try {
+      await deleteFriend(friendId);
+      await readfriendList();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-const CurrentFriendComponent = ({ friendList }) => {
+  //useEffect hook
+  useEffect(() => {
+    readfriendList();
+  }, []);
+
+  //이벤트 핸들러
+  const handleDelete = async (friendId) => {
+    try {
+      await delFriend(friendId);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
-      {friendList.map((friend) => (
+      {friendList.map((friend, index) => (
         <FriendListComponent
-          key={friend.id}
-          userName={friend.userName}
+          key={index}
+          userName={friend.nickname}
           text="삭제"
+          handleDelete={() => delFriend(friend.friendId)}
         />
       ))}
     </div>
