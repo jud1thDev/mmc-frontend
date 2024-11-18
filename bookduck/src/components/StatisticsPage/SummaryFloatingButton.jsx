cf492db647d71de7f96fac93c6810597ff871fbe
yoonjin-C <cyy000123@gmail.com>
2024-11-19 01:22:18 +0900

    💄 UI: 디자인 수정사항 반영 및 애니메이션 추가

--- bookduck/src/components/StatisticsPage/SummaryFloatingButton.jsx
+++ bookduck/src/components/StatisticsPage/SummaryFloatingButton.jsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from "react";
+
 const SummaryFloatingButton = () => {
+  const [isVisible, setIsVisible] = useState(true); // 버튼 표시 여부 상태
+  const [lastScrollY, setLastScrollY] = useState(0); // 마지막 스크롤 위치
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY > lastScrollY) {
+      // 스크롤 내릴 때
+      setIsVisible(false);
+    } else {
+      // 스크롤 올릴 때
+      setIsVisible(true);
+    }
+
+    setLastScrollY(currentScrollY); // 마지막 스크롤 위치 업데이트
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
   return (
-    <div className="fixed bottom-10 flex justify-between items-center px-4 py-2.5 text-btn3 text-white  bg-[rgba(224,173,104,0.8)] w-[22.5625rem] rounded-[6.25rem] cursor-pointer">
-      요약 이미지로 내보낼 수 있어요
-      <div className="px-3 py-1 font-semibold bg-orange-400 bg-opacity-90 rounded-[20px]">
-        보러가기
+    <div
+      className={`fixed bottom-10 flex justify-between items-center px-4 py-2.5 text-btn3 text-white  [background:rgba(107,127,240,0.80)] w-[22.5625rem] rounded-[6.25rem] cursor-pointer transition-transform duration-300
+      }`}
+      style={{
+        transform: `translateY(${isVisible ? "0" : "calc(100% + 50px)"})`,
+      }}
+    >
+      요약된 이미지로 저장할 수 있어요
+      <div className="px-3 py-1 font-semibold bg-blue-400 bg-opacity-90 rounded-[20px]">
+        이미지 보기
       </div>
     </div>
   );
