b7d00cacd616e8bbc8fc9f6b9b8605479b2f1c7c
chanhee <ichanhui578@gmail.com>
2024-11-23 12:43:56 +0900

    ✨feat: 직접 등록 책 상태 변경/ 검색 도서 서재 담기/검색 도서 상태 변경

--- bookduck/src/components/SearchPage/SearchBookComponent.jsx
+++ bookduck/src/components/SearchPage/SearchBookComponent.jsx
@@ -5,24 +5,23 @@ import ButtonComponent from "../common/ButtonComponent";
 import BottomSheetModal from "../common/BottomSheetModal";
 import Divider1 from "../../components/common/Divider1";
 import ListBottomSheet from "../common/ListBottomSheet";
-import { get, patch } from "../../api/example";
+import { get, patch, post, del } from "../../api/example";
+
+const statusArr = ["읽고 싶어요", "읽고 있어요", "다 읽었어요", "중단했어요"];
+const DATA_LIMIT = 10;
 
 const SearchBookComponent = ({ search }) => {
   const navigate = useNavigate();
   const [bottomSheetShow, setBottomSheetShow] = useState(false);
   const [visible, setVisible] = useState(false);
 
-  const statusArr = ["읽고 싶어요", "읽고 있어요", "다 읽었어요", "중단했어요"];
-  const [currentState, setCurrentStatus] = useState();
-  const [selectedBookId, setSelectedBookId] = useState();
-  const [isCancel, setIsCancel] = useState(false);
-
+  const [selectedBook, setSelectedBook] = useState(null);
   const [registeredBooks, setRegisteredBooks] = useState([]);
   const [books, setBooks] = useState([]);
+  const [currentState, setCurrentState] = useState(null);
   const [currentPage, setCurrentPage] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
   const loaderRef = useRef(null);
-  const DATA_LIMIT = 10;
 
   const getReadingStatusKey = (status) => {
     switch (status) {
@@ -42,31 +41,26 @@ const SearchBookComponent = ({ search }) => {
   //API연결
   //API-등록 책 정보받기
   const getRegisteredBooks = async (keyword, page = 0) => {
+    setRegisteredBooks([]);
+    if (!keyword) return;
     try {
       const response = await get(
         `/bookinfo/search/custom?keyword=${encodeURIComponent(
           keyword
         )}&page=${page}&size=${DATA_LIMIT}`
       );
-      console.log("response", response);
-      const data = response.bookList.map((book) => ({
-        bookinfoId: book.bookinfoId,
-        title: book.title,
-        author: book.author,
-        imgPath: book.imgPath,
-        myRating: book.myRating,
-        readStatus: book.readStatus,
-      }));
-      console.log("data", data);
-
-      setRegisteredBooks((b) => [...b, data]);
-      console.log("registeredBooks:", registeredBooks);
+      console.log("등록 책 response", response);
+      const data = response.bookList;
+      setRegisteredBooks((b) => [...b, ...data]);
+      s;
     } catch (error) {
       console.error("등록 책 읽어오기 오류", error);
     }
   };
+
   // API-일반 책 받아오기
   const getBooks = async (keyword, page = 0) => {
+    setBooks([]);
     if (!keyword) return;
     try {
       const response = await get(
@@ -74,29 +68,108 @@ const SearchBookComponent = ({ search }) => {
           keyword
         )}&page=${page}&size=${DATA_LIMIT}`
       );
-      console.log(response);
-      const data = response.bookList.map((book) => ({
-        id: book.bookinfoId,
-        title: book.title,
-        author: book.author,
-        img: book.imgPath,
-        myRating: book.myRating,
-        readStatus: book.readStatus,
-      }));
-
+      console.log("일반 책 response", response);
+      const data = response.bookList;
       setBooks((b) => [...b, ...data]);
-      setTotalPages(response.totalPages || 0); // 서버에서 전체 페이지 수 반환
+      setTotalPages(response.totalPages || 0);
     } catch (error) {
       console.error("책 데이터 불러오기 오류:", error);
     }
   };
 
-  //API-직접 등록 책 상태 변경
-  const patchRegisteredStatus = async (selectedBookId, currentStatus) => {
+  //API-책 상태 변경
+  const patchRegisteredStatus = async (option) => {
     try {
-      await patch(`/books/${selectedBookId}?status=${currentStatus}`);
+      const statusKey = getReadingStatusKey(option);
+      await patch(`/books/${selectedBook.userbookId}?status=${statusKey}`);
+      console.log(`책 상태 업데이트 성공: ${statusKey}`);
+      setSelectedBook(null);
+      getRegisteredBooks(search, 0);
+      getBooks(search, 0);
+      setVisible(false);
+      setTimeout(() => {
+        setBottomSheetShow(false);
+      }, 200);
+    } catch (error) {
+      console.error("책 상태 변경 업데이트 오류", error);
+    }
+  };
+
+  //API-서재에 책 등록
+  const postBooks = async (option) => {
+    const statusKey = getReadingStatusKey(option);
+    try {
+      const requestBody = {
+        title: selectedBook.title,
+        author: selectedBook.author,
+        imgPath: selectedBook.imgPath,
+        readStatus: statusKey,
+      };
+      await post(`/bookinfo/${selectedBook.providerId}/add`, requestBody);
+      console.log(`서재에 도서 추가 완료: ${selectedBook.providerId}`);
+      setVisible(false);
+      setTimeout(() => {
+        setBottomSheetShow(false);
+      }, 200);
+      setSelectedBook(null);
+      getBooks(search, 0);
+    } catch (error) {
+      console.error("책 등록 업데이트 오류", error);
+    }
+  };
+
+  const deleteBook = async (userbookId) => {
+    try {
+      await del(`/books/${userbookId}`);
+      setSelectedBook(null);
+      getRegisteredBooks(search, 0);
+      getBooks(search, 0);
     } catch (error) {
       console.error(error);
+    }
+  };
+
+  //이벤트 핸들러
+  // 등록된 책은 book.userbookId로 받고,
+  const handleSelectedBook = (book, isRegistered) => {
+    console.log("북덕", book);
+    const isLibraryBook = !isRegistered && book.bookUnitDto?.userbookId;
+    setSelectedBook({
+      bookInfoId: isRegistered
+        ? book.bookInfoId
+        : isLibraryBook
+        ? book.bookUnitDto.bookInfoId
+        : null,
+      userbookId: isRegistered
+        ? book.userbookId
+        : isLibraryBook
+        ? book.bookUnitDto.userbookId
+        : null,
+      providerId: isRegistered ? null : book.providerId,
+      title: isRegistered ? book.title : book.bookUnitDto.title,
+      author: isRegistered ? book.author : book.bookUnitDto.author,
+      imgPath: isRegistered ? book.imgPath : book.bookUnitDto.imgPath,
+      readStatus: isRegistered ? book.readStatus : book.bookUnitDto.readStatus,
+    });
+
+    setBottomSheetShow(true);
+  };
+  useEffect(() => {
+    console.log(selectedBook);
+  }, [selectedBook]);
+
+  const handleStatusChange = (option) => {
+    if (selectedBook.providerId) {
+      /*일반 책인 경우*/
+      /*1-미등록: readStatus가 null일 경우*/
+      if (selectedBook.readStatus === null) {
+        postBooks(option);
+      } else {
+        patchRegisteredStatus(option);
+      }
+      /*2-등록: readStatus가 존재할 경우*/
+    } else {
+      patchRegisteredStatus(option);
     }
   };
 
@@ -104,13 +177,15 @@ const SearchBookComponent = ({ search }) => {
   // 검색어 변경 시 데이터 초기화 및 첫 페이지 호출
   useEffect(() => {
     if (search) {
-      setBooks([]); // 기존 데이터 초기화
-      setCurrentPage(0); // 첫 페이지로 초기화
+      setCurrentPage(0);
       getBooks(search, 0);
-      setRegisteredBooks([]);
       getRegisteredBooks(search);
     }
   }, [search]);
+
+  useEffect(() => {
+    console.log("registered", registeredBooks);
+  }, [registeredBooks]);
 
   // 무한 스크롤 감지
   useEffect(() => {
@@ -141,43 +216,6 @@ const SearchBookComponent = ({ search }) => {
     }
   }, [currentPage, search]);
 
-  //이벤트 핸들러
-  const handleStatusClick = (id) => {
-    console.log("id", id);
-    setSelectedBookId(id);
-
-    console.log("id", selectedBookId);
-    setBottomSheetShow(true);
-  };
-
-  /*직접 등록 책 처음 등록*/
-  const handleStatusRegister = async (status) => {
-    setCurrentStatus(status);
-    const res = await patchRegisteredStatus(
-      selectedBookId,
-      getReadingStatusKey(status)
-    );
-    console.log(res);
-    setVisible(false);
-    setTimeout(() => {
-      setBottomSheetShow(false);
-    }, 200);
-  };
-
-  /*직접 등록 책 상태 변경*/
-  const handleStatusChange = async (status) => {
-    setCurrentStatus(status);
-    const res = await patchRegisteredStatus(
-      selectedBookId,
-      getReadingStatusKey(status)
-    );
-    console.log(res);
-    setVisible(false);
-    setTimeout(() => {
-      setBottomSheetShow(false);
-    }, 200);
-  };
-
   return (
     <>
       {registeredBooks.length > 0 || books.length > 0 ? (
@@ -192,10 +230,11 @@ const SearchBookComponent = ({ search }) => {
                   author={book.author}
                   bookImg={book.imgPath}
                   status={book.readStatus}
+                  rating={book.myRating}
                   register={true}
                   edit={true}
                   bottomSheet={true}
-                  handleStatusClick={() => handleStatusClick(book.bookinfoId)}
+                  handleStatusClick={() => handleSelectedBook(book, true)}
                 />
               ))}
           </div>
@@ -206,14 +245,14 @@ const SearchBookComponent = ({ search }) => {
               books.map((book, index) => (
                 <BookListView
                   key={index}
-                  bookTitle={book.title}
-                  author={book.author}
-                  bookImg={book.img}
-                  rating={book.rating}
-                  status={currentState}
+                  bookTitle={book.bookUnitDto.title}
+                  author={book.bookUnitDto.author}
+                  bookImg={book.bookUnitDto.imgPath}
+                  rating={book.bookUnitDto.myRating}
+                  status={book.bookUnitDto.readStatus}
                   edit={true}
                   bottomSheet={true}
-                  handleStatusClick={() => handleStatusClick(book.userBookdId)}
+                  handleStatusClick={() => handleSelectedBook(book, false)}
                 />
               ))}
           </div>
@@ -252,7 +291,7 @@ const SearchBookComponent = ({ search }) => {
             options={statusArr}
             currentOption={currentState}
             handleOption={handleStatusChange}
-            isCancel={isCancel}
+            handlePutCancel={deleteBook}
           />
         </div>
       </BottomSheetModal>
