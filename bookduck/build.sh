10df5f30df4d4ab46b54898491f6e72e109e9ac9
YoungseoChoi23 <dudtj9326@ewhain.net>
2024-11-30 11:53:23 +0900

    build : 빌드 파일 생성

---
+++ bookduck/build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#!/bin/sh
+
+# 프로젝트 루트 디렉토리로 이동 (스크립트 실행 위치를 기준으로)
+cd "$(dirname "$0")/.." || exit 1
+
+# 기존 output 디렉터리 삭제 (있을 경우)
+rm -rf output
+
+# output 디렉터리 생성
+mkdir output
+
+# Vite 빌드 실행
+yarn vite build || exit 1
+
+# 빌드 결과물을 output 디렉터리로 이동
+cp -R ./dist/* ./output
+
+echo "빌드 성공. Output directory created at ./output."
