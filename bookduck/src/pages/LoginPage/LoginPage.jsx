ec4f87d247496981e130bf9085b79b93a25ffff5
chanhee <ichanhui578@gmail.com>
2024-12-22 07:20:43 +0900

    ✏️fix: 비율 조정

--- bookduck/src/pages/LoginPage/LoginPage.jsx
+++ bookduck/src/pages/LoginPage/LoginPage.jsx
@@ -5,8 +5,8 @@ import google from "../../assets/loginPage/google.svg";
 
 const LoginPage = () => {
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-col h-[36.875rem] bg-gray-10 px-5">
+    <div className="h-screen overflow-hidden flex flex-col">
+      <div className="flex flex-col h-[60%] bg-gray-10 px-5">
         <img
           src={mainDuck}
           className="w-[12.75rem] h-[12.375rem] mx-auto mt-[10.31rem] mb-[6rem]"
@@ -14,7 +14,7 @@ const LoginPage = () => {
         <p className="text-t1 font-semibold mb-[0.37rem]">환영합니다!</p>
         <p className="text-b1">북덕과 함께 책을 읽고 기록을 쌓아보세요</p>
       </div>
-      <div className="flex flex-col gap-3 items-center h-[16.37rem] bg-white">
+      <div className="flex h-[40%] flex-col gap-3 items-center bg-white">
         <a
           href={GoogleURI}
           className="flex justify-center items-center gap-[0.62rem] w-[22.0625rem] h-[3rem] rounded-[0.5rem] border-solid border-[0.0938rem] border-[#ADADAD] bg-white mt-[1.69rem]"
