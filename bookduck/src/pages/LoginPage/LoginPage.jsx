1335ed0453956530c7f310106ecb24c495c28a5d
chanhee <ichanhui578@gmail.com>
2024-12-22 07:26:39 +0900

    💄ui: 비율 재조정

--- bookduck/src/pages/LoginPage/LoginPage.jsx
+++ bookduck/src/pages/LoginPage/LoginPage.jsx
@@ -6,15 +6,17 @@ import google from "../../assets/loginPage/google.svg";
 const LoginPage = () => {
   return (
     <div className="h-screen overflow-hidden flex flex-col">
-      <div className="flex flex-col h-[60%] bg-gray-10 px-5">
+      <div className="flex flex-col  bg-gray-10 px-5">
         <img
           src={mainDuck}
-          className="w-[12.75rem] h-[12.375rem] mx-auto mt-[10.31rem] mb-[6rem]"
+          className="w-[12.75rem] h-[12.375rem] mx-auto mt-[10rem] mb-[5rem]"
         />
         <p className="text-t1 font-semibold mb-[0.37rem]">환영합니다!</p>
-        <p className="text-b1">북덕과 함께 책을 읽고 기록을 쌓아보세요</p>
+        <p className="text-b1 mb-[2rem]">
+          북덕과 함께 책을 읽고 기록을 쌓아보세요
+        </p>
       </div>
-      <div className="flex h-[40%] flex-col gap-3 items-center bg-white">
+      <div className="flex flex-col gap-3 items-center bg-white">
         <a
           href={GoogleURI}
           className="flex justify-center items-center gap-[0.62rem] w-[22.0625rem] h-[3rem] rounded-[0.5rem] border-solid border-[0.0938rem] border-[#ADADAD] bg-white mt-[1.69rem]"
